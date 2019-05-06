3a86cbcf64d0c3ac9c65fcedfcff628b1d7fe7ee
Nicholas Ewing <nicholassewing@gmail.com>
2019-05-06 21:21:52 +0000

    Remove /create route for production and add notes

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const CardController = require("../controllers/cardController");
-const multer = require("multer");
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "./public/images");
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.originalname);
-	}
-});
-const upload = multer({storage: storage});
 
-/* GET home page. */
+// *****************************************************
+// You can re-enable multer and the /create route for
+// local development when you clone this repo.
+// Use /create to add cards, card images, or other 
+// information.
+
+// const multer = require("multer");
+// const storage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, "./public/images");
+// 	},
+// 	filename: (req, file, cb) => {
+// 		cb(null, file.originalname);
+// 	}
+// });
+// const upload = multer({storage: storage});
+
+// *****************************************************
+
 router.route("/")
 	.get(CardController.getCard)
 	.post(upload.single("specialHuntTileImg"), CardController.createCard);
 
-router.route("/create")
-	.get(CardController.getCreationForm)
+// router.route("/create")
+// 	.get(CardController.getCreationForm)
 
 module.exports = router;
